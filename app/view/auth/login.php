2cfd93719c598f5fb28539d086a3ee0ccd8e592f
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-01-23 16:31:17 -0500

    registro_usuario

--- app/view/auth/login.php
+++ app/view/auth/login.php
@@ -2,16 +2,31 @@
 require_once(__DIR__ . '/../layouts/header.php');
 ?>
 <div class="contenedor">
+    <div>
+        <h2>Unete a nosotros</h2>
+    </div>
     <form action="" class="">
-        <label for="nombre">Nombre:</label>
-        <input type="text" name="nombre" id="nombre" placeholder="ingrese nombre">
+        <label for="nombre">Usuario:</label>
+        <div class="icon-contenedor">
+            <i class="fa-regular fa-user"></i>
+            <input type="text" name="nombre" id="nombre" placeholder="ingrese nombre">
+        </div>
+        
 
         <label for="Correo">Correo Electronico:</label>
-        <input type="email" name="Correo" id="Correo" placeholder="ingrese Correo Electronico">
+        <div class="icon-contenedor">
+            <i class="fa-solid fa-at"></i>
+            <input type="email" name="Correo" id="Correo" placeholder="correo@gmail.com">
+        </div>
 
         <label for="password">Contraseña:</label>
-        <input type="password" name="password" id="password" placeholder="ingrese Contraseña">
+        <div class="icon-contenedor">
+            <i class="fa-solid fa-lock"></i>
+            <input type="password" name="password" id="password" placeholder="*********">
+        </div>
 
-        <input type="submit" value="Enviar">
+        <input type="submit" value="Registrar">
+
+        <a href="">Ya tienes cuenta? Inicia sesion</a>
     </form>
 </div>
--- app/view/layouts/header.php
+++ app/view/layouts/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>inicio</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@5.15.4/css/all.min.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="/../../../public/css/estilonav.css">
 </head>
 
@@ -19,7 +19,7 @@
         <nav>
             <ul>
                 <li><a href="#">Inicio</a></li>
-                <li><a href="#">Sobre Mi</a></li>
+                <li><a href="#">Producto</a></li>
                 <li><a href="#">Contacto</a></li>
                 <li><a href="#">Publicacion</a></li>
             </ul>
