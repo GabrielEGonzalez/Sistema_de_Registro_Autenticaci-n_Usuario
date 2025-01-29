47b224aee7b6b3f3fbb0132fb051e37b025d32e2
GabrielEGonzalez <enriquegonzalez123467@gmail.com>
2025-01-29 15:10:38 -0500

    registro_usuario

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 require_once(__DIR__ . '/../app/controller/controlerhome.php');
 require_once(__DIR__ . '/../app/controller/controllerRegistro.php');
+
 $url = $_SERVER['REQUEST_URI'];
 $metodo = $_SERVER['REQUEST_METHOD'];
 
@@ -19,13 +20,11 @@ if ($url === '/public/') {
   if (isset($_POST['nombre']) && isset($_POST['Correo']) && isset($_POST['password'])) {
     $registro = new ControllerRegistro();
     $registro->registro();
-  }elseif(isset($_POST['correo']) && isset($_POST['password'])){
+  } elseif (isset($_POST['correo']) && isset($_POST['password'])) {
     $registro = new ControllerRegistro();
     $registro->login();
   }
-
-
-}elseif($url === '/public/inicio/home'){
+} elseif ($url === '/public/inicio/home') {
   $inicio = new ControllerRegistro();
   $inicio->home();
 
